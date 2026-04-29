9f700fb0012e1daa36964dc1d40d6dc985bf10c8
Gutan Marina <gutanmarina123@gmail.com>
2026-04-29 12:42:43 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/carrentalmanagementtmppp/CarRentalManagementTmpppApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalmanagementtmppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalManagementTmpppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalManagementTmpppApplication.class, args);
+    }
+
+}
